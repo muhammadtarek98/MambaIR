d216fcb16ad24b60c15f308b72fa5f3b8458f49b
muahmmad <muahmmadtarek@git>
2024-09-09 03:12:22 +0300

    fix WaveNet basicsr repo and run Water net inference repo

--- basicsr/version.py
+++ basicsr/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Sun Aug 25 20:36:47 2024
+# TIME: Sun Sep  8 22:47:24 2024
 __version__ = '1.0.0'
-__gitsha__ = '3fabeac'
+__gitsha__ = 'a4d2b7b'
 version_info = (1, 0, 0)
